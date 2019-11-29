88039bac308ffac93da785eacb57ef4d74baba9a
Pablo Cardozo <pablocyc83@gmail.com>
2019-11-29 03:09:02 -0400

    leyendo boton touch, menu de 5 opciones por terminal

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,5 +1,15 @@
+// Constants
 #define pinLed      LED_BUILTIN
-#define pinButton   D3
+#define pinButton   D8
+#define option_limit  5
+
+// Variables
+String option_message = "Opcion: ";
+bool option_array[option_limit];
+int option_position;
+unsigned long current_time;
+unsigned long double_click_time = 700;
+bool press_active;
 
 void setup () {
   pinMode(pinLed, OUTPUT);
@@ -9,15 +19,42 @@ void setup () {
 }
 
 void loop () {
-  if (!digitalRead(pinButton)) {
-    int value = digitalRead(pinLed);
-    digitalWrite(pinLed, !value);
-    Serial.print("LED: ");
-    if (value)
-      Serial.println("ON");
-    else
-      Serial.println("OFF");
-    
-    while(!digitalRead(pinButton));
+  ReadButton();
+}
+
+void ReadButton () {
+  if (digitalRead(pinButton)) {
+    while(digitalRead(pinButton));
+
+    if (!press_active) {
+      current_time = millis();
+      press_active = true;
+    }
+    else {
+      ledToggle(option_position);
+      press_active = false;
+    }
   }
+  
+  if (millis() > current_time + double_click_time) {
+    if (press_active)
+      showOptions();
+    press_active = false;
+  }
+}
+
+void ledToggle (int pos) {
+  option_array[pos] = !option_array[pos];
+  int value = option_array[pos];
+  if (value)
+    Serial.println(String(pos) + ": ON");
+  else
+    Serial.println(String(pos) + ": OFF");
+}
+
+void showOptions () {
+  option_position++;
+  if (option_position > option_limit - 1)
+    option_position = 0;
+  Serial.println(option_message + String(option_position));
 }
