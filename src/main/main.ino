4f470d8e18f7c48d59377ecc77fbc18c83f615ec
Pablo Cardozo <pablocyc83@gmail.com>
2019-12-02 19:53:51 -0400

    montando webserver, y mostrando el estado del led actual en local web

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,7 +1,18 @@
 // Libraries
 #include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 
 // Constants
+#ifndef STASSID
+#define STASSID "Network_Cardozo"
+#define STAPSK  "Card0z0Ch1l3"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
 #define pinPixel      D4
 #define numPixels     5
 #define pinButton     D8
@@ -14,20 +25,75 @@ int option_position;
 unsigned long current_time;
 unsigned long double_click_time = 700;
 bool press_active;
+String message_output;
 
 // Init
 Adafruit_NeoPixel pixels(numPixels, pinPixel, NEO_GRB + NEO_KHZ800);
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/html", message_output);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
 
 void setup () {
   pinMode(pinButton, INPUT);
   pixels.begin();
   Serial.begin(9600);
-  Serial.println("WebServer - nodemcu");
   pixels.clear();
+  pixels.show();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/digital", []() {
+    server.send(200, "text/html", "<h1>this works as well...</h1>");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP WebServer - nodemcu");
+  messagePrint(0, "APAGADO");
 }
 
 void loop () {
   ReadButton();
+  server.handleClient();
+  MDNS.update();
 }
 
 void ReadButton () {
@@ -55,15 +121,24 @@ void ledToggle (int pos) {
   option_array[pos] = !option_array[pos];
   int value = option_array[pos];
   if (value) {
+    messagePrint(pos, "ENCENDIDO");
     Serial.println(String(pos) + ": ON");
     pixels.setPixelColor(pos, pixels.Color(170, 0, 255));
     pixels.show();
   }
   else {
+    messagePrint(pos, "APAGADO");
     Serial.println(String(pos) + ": OFF");
     pixels.setPixelColor(pos, pixels.Color(0, 0, 0));
     pixels.show();
   }
+}
+
+void messagePrint(int pos, String status) {
+  message_output = "<h1>LEDs Status</h1>";
+  message_output += "<ul>";
+  message_output += "<li>LED " + String(pos) + ": " + status + "</li>";
+  message_output += "</ul>";
 }
 
 void showOptions () {
